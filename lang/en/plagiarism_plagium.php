ad2b58f0f0e37c735b6a959b4f779d4189dc997b
Septet Systems <guillaumeherisson@gmail.com>
2023-03-22 14:03:05 -0400

    localization

---
+++ lang/en/plagiarism_plagium.php
@@ -0,0 +1,32 @@
+<?php
+
+$string['pluginname'] = "Plagium";
+$string['api_username'] = "Username";
+$string['api_password'] = "Password";
+$string['api_key'] = "Api Key";
+$string['api_server'] = 'Api Url';
+$string['api_server_help'] = 'Plagium server';
+
+$string['api_analyze'] = 'Analyze';
+$string['api_analyze_automatic'] = 'Auto';
+$string['api_analyze_manual'] = 'Manual';
+
+$string['api_seach_by_default'] = 'Search Type';
+$string['api_seach_by_default_quick'] = 'Quick Search';
+$string['api_seach_by_default_search'] = 'Deep Search';
+
+$string['api_seach_type'] = 'Source';
+$string['api_seach_type_web'] = 'Web';
+$string['api_seach_type_file'] = 'File';
+
+$string['config_info1'] = '<b class="plagium-badge">Plagium</b> is a paid plagiarism detection software that is designed to help individuals and organizations detect instances of plagiarism in written content. It uses advanced algorithms and technologies to compare submitted content against a vast database of sources, including academic journals, websites, and other online sources.';
+$string['config_info2'] = 'You can find more information at <a href="https://www.plagium.com/en/moodle">https://www.plagium.com/en/moodle</a>.';
+$string['config_info3'] = 'General information can be found at <a href="https://www.plagium.com">https://www.plagium.com</a>.';
+
+$string['action_analyze'] = 'Analyze';
+$string['action_similarity'] = 'Similarity';
+$string['action_risk'] = 'Risk';
+$string['action_similarity_max'] = 'Similarity Max.';
+$string['action_report'] = 'Report';
+$string['action_pdf'] = 'PDF';
+$string['action_full_report'] = 'Full Report';
---
+++ lang/es/plagiarism_plagium.php
@@ -0,0 +1,32 @@
+<?php
+
+$string['pluginname'] = "Plagium";
+$string['api_username'] = "Nombre de usuario";
+$string['api_password'] = "Contraseña";
+$string['api_key'] = "Clave API";
+$string['api_server'] = 'Api URL';
+$string['api_server_help'] = 'Servidor Plagium';
+
+$string['api_analyze'] = 'Analizar';
+$string['api_analyze_automatic'] = 'Auto';
+$string['api_analyze_manual'] = 'Manual';
+
+$string['api_seach_by_default'] = 'Tipo de búsqueda';
+$string['api_seach_by_default_quick'] = 'Búsqueda rápida';
+$string['api_seach_by_default_search'] = 'Búsqueda profunda';
+
+$string['api_seach_type'] = 'Fuente';
+$cadena['api_seach_type_web'] = 'Web';
+$string['api_seach_type_file'] = 'Archivo';
+
+$string['config_info1'] = '<b class="plagium-badge">Plagium</b> es un software de detección de plagio de pago que está diseñado para ayudar a las personas y organizaciones a detectar instancias de plagio en el contenido escrito. Utiliza algoritmos y tecnologías avanzados para comparar el contenido enviado con una amplia base de datos de fuentes, incluidas revistas académicas, sitios web y otras fuentes en línea.';
+$string['config_info2'] = 'Puede encontrar más información en <a href="https://www.plagium.com/en/moodle">https://www.plagium.com/en/moodle</ a>.';
+$string['config_info3'] = 'La información general se puede encontrar en <a href="https://www.plagium.com">https://www.plagium.com</a>.';
+
+$string['action_analyze'] = 'Analizar';
+$string['action_similarity'] = 'Similitud';
+$string['action_risk'] = 'Riesgo';
+$string['action_similarity_max'] = 'Similitud Máx.';
+$string['action_report'] = 'Informe';
+$cadena['action_pdf'] = 'PDF';
+$string['action_full_report'] = 'Informe completo';
