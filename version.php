3be1fe5be1364ef9be6e8b9edf98b8e7cd710704
Vinicius brito <vinicius.brito@clinicarx.com.br>
2023-03-23 19:11:13 -0300

    version

--- version.php
+++ version.php
@@ -20,4 +20,4 @@ $plugin->requires = 2019101000;
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'plagiarism_plagium';
-$plugin->release = '3.2.2';
+$plugin->release = '1.0.0';
