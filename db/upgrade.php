f43f7eb8eb7da827511f7bd5efb4087a45c51ae4
Septet Systems <guillaumeherisson@gmail.com>
2023-03-24 11:16:39 -0400

    adding composer

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -24,7 +23,10 @@
  * @copyright    @2015 plagium GmbH
  * @license      http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-function xmldb_plagiarism_plagium_upgrade($oldversion) {
+
+
+function xmldb_plagiarism_plagium_upgrade($oldversion) 
+{
     global $DB;
 
     $dbman = $DB->get_manager();
