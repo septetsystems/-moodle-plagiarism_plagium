dbcce60458f25c144c8fcfe6e4353eff15302e3f
Vinicius brito <vinicius.brito@clinicarx.com.br>
2023-03-29 17:47:27 -0300

    removendo comentarios

--- db/access.php
+++ db/access.php
@@ -1,29 +1,5 @@
 <?php
 
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * settings.php - allows the admin to configure plagiarism stuff
- *
- * @package     plagiarism_plagium
- * @subpackage  plagiarism
- * @author      Daniel Gockel  <dgockel@plagium.com>
- * @copyright   2015 plagium GmbH {@link https://www.plagium.com/}
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 $capabilities = array(
     //capability to enable/disable plagium inside an activity
     'plagiarism/plagium:enable' => array(
