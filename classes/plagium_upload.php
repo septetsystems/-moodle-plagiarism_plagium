9eeb3d84f8865f3b7b2be64ac3e72dd9744f3414
Septet Systems <473454+septetsystems@users.noreply.github.com>
2023-03-29 19:15:34 -0400

    file headers corrections  and copyright information

--- classes/plagium_upload.php
+++ classes/plagium_upload.php
@@ -1,24 +1,24 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of the Plagium Plugin for Moodle - https://www.plagium.com
 //
-// Moodle is free software: you can redistribute it and/or modify
+// The Plagium Plugin for Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Moodle is distributed in the hope that it will be useful,
+// The Plagium Plugin for Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with the Plagium Plugin for Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Contains agreement class form.
  *
  * @package   plagiarism_plagium
- * @copyright 2023 Catalyst IT
+ * @copyright 2023 Septet Systems
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
