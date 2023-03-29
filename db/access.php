699a16e570b3899a3b5fc1bd63cc5531c1f652cf
Vinicius brito <vinicius.brito@clinicarx.com.br>
2023-03-29 18:37:03 -0300

    Ajustes

--- db/access.php
+++ db/access.php
@@ -22,6 +22,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 $capabilities = array(
     //capability to enable/disable plagium inside an activity
     'plagiarism/plagium:enable' => array(
