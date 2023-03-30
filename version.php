f13743dcaf240cf4d8aeb16839b5d6fbb51f4f93
Septet Systems <473454+septetsystems@users.noreply.github.com>
2023-03-29 20:53:26 -0400

    correction comment version

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-// Plugin Release Date
+// Plugin Release Date.
 $plugin->version = 2023032900;
-// Moodle 3.7
+// Moodle 3.7.
 $plugin->requires = 2019101000;
 $plugin->cron     = 0;
 $plugin->maturity = MATURITY_STABLE;
